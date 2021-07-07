c7a9be70de10225c697116036f2fc8cd9127c6a1
sandycfisher <1801486@uad.ac.uk>
2021-07-07 15:42:36 +0100

    Caterpillar movement
    Implemented caterpillar movement. There is a bug where the compartments do uncouple. I'll try to get that fixed

--- Dare Academy/Assets/Res/Scripts/Enemies/CaterpillerEnemy.cs
+++ Dare Academy/Assets/Res/Scripts/Enemies/CaterpillerEnemy.cs
@@ -6,91 +6,153 @@ using JUtil;
 
 public class CaterpillerEnemy : GridEntity
 {
-    [SerializeField] private GridEntity m_followEntity;
-    public GridEntity m_behindEntity;
-    [SerializeField] private bool head;
+    [SerializeField] private GridEntity m_followEntity; // The next compartment or the player that the enemy is going to be following
+    [SerializeField] private bool head; // Variable to tell which compartment is at the front
 
-    private Vector2 m_dir = new Vector2();
-    [SerializeField] private int m_moveSpeed = 1;
+    private Vector2 m_dir = new Vector2(); // Direction for figuring out movement
+    [SerializeField] private int m_moveSpeed = 1; // This enemy will move one square for each step
 
     private void OnValidate()
     {
-        if (m_followEntity && m_followEntity.TryGetComponent(out CaterpillerEnemy caterpiller))
+        if (m_followEntity && m_followEntity.TryGetComponent(out CaterpillerEnemy caterpiller)) // If the following entity has the caterpillar script
         {
-            caterpiller.m_behindEntity = this;
-            head = false;
+            head = false; // Then it is not at the front
         }
-        else
+        else // If it doesn't. aka its following the player
         {
-            head = true;
+            head = true; // Then this compartment is the front of the caterpillar
         }
     }
 
     protected override void Start()
     {
-        base.Start();
-        m_health = 5;
+        base.Start(); // Run base application
+        m_health = 5; // Set health of compartment to 5
         m_flags.SetFlags(GridEntityFlags.Flags.isKillable, true);
         m_flags.SetFlags(GridEntityFlags.Flags.isSolid, true);
     }
 
     public override void AnalyseStep()
     {
-        if (m_followEntity.Position != null)
+
+        Vector3[] path; // Declare paht variable
+
+        Vector3 dir = Vector3.zero; // Declare temporary direction variable
+
+        if (!head) // If this compartment is not the front
         {
-            Vector3[] path = App.GetModule<LevelModule>().MetaGrid.GetPath(Position.world, m_followEntity.Position.world);
-
-            Vector3 dir = new Vector3();
-
-            if (path.Length > 1)
+            // MIGHT WANT TO CHANGE THIS FOR BURROWING MECHANIC
+            Vector2 followingNode = new Vector2(Position.world.x, Position.world.y); // Failsafe assigning (line 49). Essentially if the front of the caterpillar is gone the rest of the compartments should just stop moving
+            
+            if (m_followEntity != null) // If the node exists on the grid
             {
-                dir = path[1] - path[0];
+                followingNode = m_followEntity.transform.position; // Find the position of the node it is going to be following
             }
-            else
-            {
-                Vector2 temp = m_followEntity.Position.grid - Position.grid;
-                dir = new Vector3(temp.x, temp.y, 0);
-            }
-
-            m_dir = new Vector2Int((int)dir.x, (int)dir.y);
+            Vector2 temp = followingNode - new Vector2(Position.world.x, Position.world.y); // Find direction for which way the compartment is meant to be moving
+            dir = new Vector3(temp.x, temp.y, 0); // Set direction to value calculated above
+            dir = dir.normalized; // Normalize to values between 1 and -1
         }
-        else
+        else // If it is the front of the head
         {
-            m_dir = Vector2.zero;
+            path = App.GetModule<LevelModule>().MetaGrid.GetPath(Position.world, m_followEntity.transform.position); // Find path to player
+            
+            if (path.Length > 1) // If path is greater than 1
+            {                
+                dir = path[1] - path[0]; // Set direction to be distance vector of the two closest pathfinding nodes
+            }
+            //else
+            //{
+            //    Vector2 temp = new Vector2(m_followEntity.transform.position.x, m_followEntity.transform.position.y) - Position.grid;
+            //    dir = new Vector3(temp.x, temp.y, 0);
+            //    dir = dir.normalized;
+            //}
+
+
+            if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int((int)dir.x, (int)dir.y)), false).Count > 0) // If there are entities infront of the caterpillar
+            {
+                if (m_currentNode.GetNeighbour(new Vector2Int((int)dir.x, (int)dir.y)).position.world != m_followEntity.transform.position) // If it is not the player
+                {
+                    dir = CheckDirectionsOfMovement(dir); // Figure out a new direction of movement
+                }
+            }
+
         }
 
-        SetMovementDirection(m_dir, m_moveSpeed);
-        base.AnalyseStep();
+        m_dir = new Vector2Int((int)dir.x, (int)dir.y); // Convert direction to integer       
+        SetMovementDirection(m_dir, m_moveSpeed); // Set movement
+        base.AnalyseStep(); // Run base function
     }
 
     public override void AttackStep()
     {
-        base.AttackStep();
+        base.AttackStep(); // Run base function
     }
 
-    public override void ResolvePassThroughStep()
+    private Vector2 CheckDirectionsOfMovement(Vector2 direction)
     {
-        m_currentNode.GetGridEntities();
 
-        foreach (GridEntity entity in m_currentNode.GetGridEntities())
+        if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count > 0) // If the caterpillar can't move right
         {
-            if (entity.isPlayer)
+            if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, 1))).Count == 0) // Check if it can move up
             {
+                return new Vector2(0, 1); // If it can then return this direction
+            }
+            else if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, -1))).Count == 0) // Check if it can move down
+            {
+                return new Vector2(0, -1); // If it can then return this direction
+            }
+            else if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(-1, 0))).Count == 0) // Check if it can move left
+            {
+                return new Vector2(-1, 0); // If it can then return this direction
             }
         }
-        base.ResolvePassThroughStep();
-    }
-
-    public override void ResolveMoveStep()
-    {
-        m_currentNode.GetGridEntities();
-
-        foreach (GridEntity entity in m_currentNode.GetGridEntities())
+        else if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(-1, 0))).Count > 0) // If the caterpillar can't move left
         {
-            if (entity.isPlayer)
+            if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, 1))).Count == 0) // Check if it can move up
             {
+                return new Vector2(0, 1); // If it can then return this direction
+            }
+            else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, -1))).Count == 0) // Check if it can move down
+            {
+                return new Vector2(0, -1); // If it can then return this direction
+            }
+            else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count == 0) // Check if it can move right
+            {
+                return new Vector2(1, 0); // If it can then return this direction
             }
         }
-        base.ResolveMoveStep();
+        else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, 1))).Count > 0) // If the caterpillar can't move up
+        {
+            if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count == 0) // Check if it can move right
+            {
+                return new Vector2(1, 0); // If it can then return this direction
+            }
+            else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(-1, 0))).Count == 0) // Check if it can move left
+            {
+                return new Vector2(-1, 0); // If it can then return this direction
+            }
+            else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, -1))).Count == 0) // Check if it can move down
+            {
+                return new Vector2(0, -1); // If it can then return this direction
+            }
+        }
+        else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, -1))).Count > 0) // If the caterpillar can't move down
+        {
+            if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count == 0) // Check if it can move right
+            {
+                return new Vector2(1, 0); // If it can then return this direction
+            }
+            else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(-1, 0))).Count == 0) // Check if it can move left
+            {
+                return new Vector2(-1, 0); // If it can then return this direction
+            }
+            else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(0, 1))).Count == 0) // Check if it can move up
+            {
+                return new Vector2(0, 1); // If it can then return this direction
+            }
+        }
+
+        // LIKELY WANT TO CHANGE THIS WHEN IMPLEMENTING BURROW ACTION
+        return new Vector2(0, 0); // If there was no solution then just make the caterpillar not move in any direction
     }
 }
