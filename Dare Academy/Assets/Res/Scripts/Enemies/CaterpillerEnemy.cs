1090e95f1977921610dc8b705149e3c232b1c04c
sandycfisher <1801486@uad.ac.uk>
2021-07-08 20:51:13 +0100

    Caterpillar burrow (Unfinished)
    Implemented the start of the burrow mechanic. The head burrows but I am in the middle of implementing the rest of the compartments for burrowing using a new script called burrow manager.

--- Dare Academy/Assets/Res/Scripts/Enemies/CaterpillerEnemy.cs
+++ Dare Academy/Assets/Res/Scripts/Enemies/CaterpillerEnemy.cs
@@ -9,8 +9,12 @@ public class CaterpillerEnemy : GridEntity
     [SerializeField] private GridEntity m_followEntity; // The next compartment or the player that the enemy is going to be following
     [SerializeField] private bool head; // Variable to tell which compartment is at the front
 
-    private Vector2 m_dir = new Vector2(); // Direction for figuring out movement
+    [SerializeField] private Vector2 m_dir = new Vector2(); // Direction for figuring out movement
     [SerializeField] private int m_moveSpeed = 1; // This enemy will move one square for each step
+
+    public bool isBurrowing = false;
+    public bool isTelegraphBurrow = false;
+    public Vector3 newPos = Vector3.zero;
 
     private void OnValidate()
     {
@@ -33,16 +37,16 @@ public class CaterpillerEnemy : GridEntity
 
     public override void AnalyseStep()
     {
-
+        base.AnalyseStep(); // Run base function
         Vector3[] path; // Declare paht variable
 
         Vector3 dir = Vector3.zero; // Declare temporary direction variable
 
         if (!head) // If this compartment is not the front
         {
-            // MIGHT WANT TO CHANGE THIS FOR BURROWING MECHANIC
+            // MIGHT WANT TO CHANGE THIS FOR BURROWING MECHANIC            
             Vector2 followingNode = new Vector2(Position.world.x, Position.world.y);
-            
+
             if (m_followEntity != null) // If the entity exists on the grid
             {
                 followingNode = m_followEntity.transform.position; // Find the position of the node it is going to be following
@@ -53,33 +57,51 @@ public class CaterpillerEnemy : GridEntity
         }
         else // If it is the front of the head
         {
-            path = App.GetModule<LevelModule>().MetaGrid.GetPath(Position.world, m_followEntity.transform.position); // Find path to player
-            
-            if (path.Length > 1) // If path is greater than 1
-            {                
-                dir = path[1] - path[0]; // Set direction to be distance vector of the two closest pathfinding nodes
-            }
-            //else
-            //{
-            //    Vector2 temp = new Vector2(m_followEntity.transform.position.x, m_followEntity.transform.position.y) - Position.grid;
-            //    dir = new Vector3(temp.x, temp.y, 0);
-            //    dir = dir.normalized;
-            //}
-
-
-            if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int((int)dir.x, (int)dir.y)), false).Count > 0) // If there are entities infront of the caterpillar
+            if (!isBurrowing)
             {
-                if (m_currentNode.GetNeighbour(new Vector2Int((int)dir.x, (int)dir.y)).position.world != m_followEntity.transform.position) // If it is not the player
+                GetComponent<SpriteRenderer>().enabled = true;
+                transform.GetChild(0).gameObject.SetActive(false);
+
+                path = App.GetModule<LevelModule>().MetaGrid.GetPath(Position.world, m_followEntity.transform.position); // Find path to player
+
+                if (path.Length > 1) // If path is greater than 1
                 {
-                     dir = CheckDirectionsOfMovement(dir); // Figure out a new direction of movement                   
+                    dir = path[1] - path[0]; // Set direction to be distance vector of the two closest pathfinding nodes
+                }
+
+                if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int((int)dir.x, (int)dir.y)), false).Count > 0) // If there are entities infront of the caterpillar
+                {
+                    if (m_currentNode.GetNeighbour(new Vector2Int((int)dir.x, (int)dir.y)).position.world != m_followEntity.transform.position) // If it is not the player
+                    {
+                        dir = CheckDirectionsOfMovement(dir); // Figure out a new direction of movement                   
+                    }
+                }
+
+                if (dir == Vector3.zero)
+                {
+                    isBurrowing = true;
                 }
             }
+            else
+            {
+                
 
+                if(isBurrowing && isTelegraphBurrow)
+                {
+                    BurrowMove();
+                }
+
+                if (!isTelegraphBurrow)
+                {
+                    TelegraphBurrow();
+                }
+            }
+            
+            GetComponentInParent<BurrowManager>().UpdateBurrow();
         }
 
         m_dir = new Vector2Int((int)dir.x, (int)dir.y); // Convert direction to integer       
         SetMovementDirection(m_dir, m_moveSpeed); // Set movement
-        base.AnalyseStep(); // Run base function
     }
 
     public override void AttackStep()
@@ -90,7 +112,7 @@ public class CaterpillerEnemy : GridEntity
     private Vector2 CheckDirectionsOfMovement(Vector2 direction)
     {
 
-        if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count > 0) // If the caterpillar can't move right
+        if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count > 0) // If the caterpillar can't move right
         {
             if (m_currentNode.GetNeighbour(new Vector2Int(0, 1)) != null)
             {
@@ -114,7 +136,7 @@ public class CaterpillerEnemy : GridEntity
                 }
             }
         }
-        else if(GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(-1, 0))).Count > 0) // If the caterpillar can't move left
+        else if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(-1, 0))).Count > 0) // If the caterpillar can't move left
         {
             if (m_currentNode.GetNeighbour(new Vector2Int(0, 1)) != null)
             {
@@ -130,7 +152,7 @@ public class CaterpillerEnemy : GridEntity
                     return new Vector2(0, -1); // If it can then return this direction
                 }
             }
-            else if(m_currentNode.GetNeighbour(new Vector2Int(1, 0)) != null)
+            else if (m_currentNode.GetNeighbour(new Vector2Int(1, 0)) != null)
             {
                 if (GetEntitiesOnNode(m_currentNode.GetNeighbour(new Vector2Int(1, 0))).Count == 0) // Check if it can move right
                 {
@@ -189,5 +211,86 @@ public class CaterpillerEnemy : GridEntity
 
         // LIKELY WANT TO CHANGE THIS WHEN IMPLEMENTING BURROW ACTION
         return new Vector2(0, 0); // If there was no solution then just make the caterpillar not move in any direction
+    }
+
+    private void TelegraphBurrow()
+    {
+        transform.GetChild(0).gameObject.transform.localPosition = Vector3.zero;
+        bool foundPos = false;
+        int burrowRange = 7;
+
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        transform.GetChild(0).gameObject.SetActive(true);
+
+        while (foundPos == false)
+        {
+            newPos = new Vector3(Random.Range(transform.position.x - burrowRange, transform.position.x + burrowRange), Random.Range(transform.position.y - burrowRange, transform.position.y + burrowRange), 0);
+            newPos = new Vector3Int((int)newPos.x, (int)newPos.y, (int)newPos.z);
+            newPos = new Vector3(newPos.x + 0.5f, newPos.y + 0.5f, 0);
+
+            if (blu.App.GetModule<blu.LevelModule>().Grid(RoomIndex).NodeExistsAt(newPos))
+            {
+                Debug.Log("Node exists");
+                foundPos = true;
+            }
+        }
+
+
+        isTelegraphBurrow = true;
+    }
+
+    private void BurrowMove()
+    {
+        Vector3 distanceVector = transform.GetChild(0).gameObject.transform.position - newPos;
+
+        if(distanceVector.x > 0)
+        {
+            transform.GetChild(0).gameObject.transform.position += new Vector3(-1, 0, 0);
+            return;
+        }
+        else if(distanceVector.x < 0)
+        {
+            transform.GetChild(0).gameObject.transform.position += new Vector3(1, 0, 0);
+            return;
+        }
+        else if (distanceVector.y > 0)
+        {
+            transform.GetChild(0).gameObject.transform.position += new Vector3(0, -1, 0);
+            return;
+        }
+        else if (distanceVector.y < 0)
+        {
+            transform.GetChild(0).gameObject.transform.position += new Vector3(0, 1, 0);
+            return;
+        }
+
+        if(transform.GetChild(0).gameObject.transform.position == newPos)
+        {
+            m_currentNode.RemoveEntity(this);
+            m_currentNode = blu.App.GetModule<blu.LevelModule>().Grid(RoomIndex).WorldToNode(newPos);
+            
+            transform.position = newPos;
+
+            isTelegraphBurrow = false;
+            isBurrowing = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!isTelegraphBurrow || !isBurrowing) // If the entity is not burrowing
+        {
+            Gizmos.color = new Color(1, 1, 1, 1);
+        }
+        else // If the entity is burrowing
+        {
+            if (isBurrowing)
+            {
+                Gizmos.color = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, 1f); // Set color of boxes to be a transparent yellow
+                Gizmos.DrawLine(transform.position, newPos);
+            }
+        }
+
+
     }
 }
