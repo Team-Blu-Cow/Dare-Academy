8f575d95bfe511223cad4464e28011013c1760d1
jack leslie <60219781+JackLeslie432@users.noreply.github.com>
2021-07-20 13:40:28 +0100

    Update MiniMapGen.cs

--- Dare Academy/Assets/Res/Scripts/UI/MiniMapGen.cs
+++ Dare Academy/Assets/Res/Scripts/UI/MiniMapGen.cs
@@ -55,14 +55,15 @@ public class MiniMapGen : MonoBehaviour, IScrollHandler, IDragHandler, IBeginDra
     {
         if (open)
         {
-            App.CanvasManager.GetCanvasContainer("Map").CloseCanvas();
+            //App.CanvasManager.GetCanvasContainer("Map").CloseCanvas();
+            App.CanvasManager.CloseCanvas();
             App.GetModule<InputModule>().PlayerController.Enable();
             App.GetModule<InputModule>().SystemController.MapControlls.Disable();
             open = false;
         }
         else
         {
-            App.CanvasManager.OpenCanvas("Map");
+            App.CanvasManager.OpenCanvas("Map", true);
             App.GetModule<InputModule>().PlayerController.Disable();
             App.GetModule<InputModule>().SystemController.MapControlls.Enable();
             transform.anchoredPosition = Vector2.zero;
