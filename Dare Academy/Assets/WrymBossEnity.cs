c2e432398d37dd497a3efc4e064ccf30b435a6d7
jack leslie <60219781+JackLeslie432@users.noreply.github.com>
2021-07-20 16:57:26 +0100

    wyrm head
    Wyrm head has full functionaliity

--- Dare Academy/Assets/WrymBossEnity.cs
+++ Dare Academy/Assets/WrymBossEnity.cs
@@ -13,12 +13,20 @@ public class WrymBossEnity : GridEntity
     [SerializeField] private bool m_head;
     [SerializeField] private GridEntity m_followEntity;
 
+    private bool m_attack = false;
+    private Vector2 m_fireDirection;
+    private Vector2Int m_burrowPos;
+    List<Vector2Int> m_attackNodes = new List<Vector2Int>();
+
+
     [Header("Phase Two Variables")]
     [SerializeField] private int m_stepTimer = 0;
 
     [SerializeField] private bool m_firingPhaseTwo;
     [SerializeField] private GameObject m_bulletPrefab;
     [SerializeField] private int m_fireCooldown = 3;
+
+    private GameObject m_damageEntityPrefab;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -30,6 +38,8 @@ public class WrymBossEnity : GridEntity
         {
             m_head = true;
         }
+
+        m_damageEntityPrefab = Resources.Load<GameObject>("prefabs/Entities/DamageEntity");
     }
 
     protected override void OnValidate()
@@ -42,22 +52,50 @@ public class WrymBossEnity : GridEntity
     {
         if (m_player == null)
             return;
+
+        if (m_burrowPos != Vector2Int.zero)
+        {
+            var currentRoom = App.GetModule<LevelModule>().CurrentRoom;
+
+            RemoveFromCurrentNode();
+
+            if (currentRoom[m_burrowPos].GetGridEntities().Count < 1)
+            {
+                m_currentNode = currentRoom[m_burrowPos];
+                transform.position = currentRoom[m_burrowPos].position.world;
+            }
+
+            AddToCurrentNode();
+
+            m_burrowPos = Vector2Int.zero;
+        }
+
         Phase1();
-        if (Health > 5)
-        {
-        }
-        else if (m_stepTimer < 100) // TODO: add phase 2 timer
-        {
-            Phase2();
-        }
-        else // Phase 3
-        {
-        }
+
+        //if (Health > 5)
+        //{
+        //}
+        //else if (m_stepTimer < 100) // TODO: add phase 2 timer
+        //{
+        //    Phase2();
+        //}
+        //else // Phase 3
+        //{
+        //}
     }
 
     public override void DamageStep()
     {
         base.DamageStep();
+        if (m_attack)
+        {
+            m_attack = false;
+            foreach (var node in m_attackNodes)
+            {
+                GameObject obj = Instantiate(m_damageEntityPrefab, App.GetModule<LevelModule>().CurrentRoom.ToWorld(node), Quaternion.identity);
+                obj.GetComponent<DamageEntity>().Countdown = 1;
+            }
+        }
     }
 
     private void Phase1()
@@ -67,19 +105,19 @@ public class WrymBossEnity : GridEntity
 
         Vector3[] path = App.GetModule<LevelModule>().MetaGrid.GetPath(Position.world, m_player.transform.position); // Find path to player
 
-        if (path.Length > 1) // If path is greater than 1
+        if (Vector2Int.Distance(Position.grid, m_player.Position.grid) <= 1)
         {
-            dir = path[1] - path[0]; // Set direction to be distance vector of the two closest pathfinding nodes
-        }
-        else if (path.Length <= 1)
-        {
-            //attack
-            Debug.Log("Wyrm attacking");
-            FireAttack(dir);
+            Vector2 temp = Position.grid - m_player.Position.grid;
+            m_attack = true;
+            FireAttack(temp.normalized);
+
             Burrow();
         }
-
-        SetMovementDirection(dir, m_moveSpeed); // Set movement
+        else if (path != null && path.Length > 1)
+        {
+            dir = path[1] - path[0]; // Set direction to be distance vector of the two closest pathfinding nodes
+            SetMovementDirection(dir, m_moveSpeed); // Set movement
+        }
     }
 
     private void Phase2()
@@ -153,24 +191,41 @@ public class WrymBossEnity : GridEntity
     private void FireAttack(Vector2 dir)
     {
         // Spawn one in direction
+        m_attackNodes.Clear();
 
-        int sign = 1; // Goten from the direction
+        m_attackNodes.Add(Position.grid - new Vector2Int((int)dir.x, (int)dir.y));
 
-        //facing right
-        for (int i = -1; i < 1; i++)
+        if (Mathf.Abs(dir.x) > 0)
         {
-            for (int j = 2; j < 3; j++)
+            //facing right
+            for (int i = -1; i < 2; i++)
             {
-                var spawnPos = Position.grid + new Vector2Int(j * sign, i);
+                for (int j = 2; j < 4; j++)
+                {
+                    if (App.GetModule<LevelModule>().CurrentRoom[Position.grid + new Vector2Int(j * -(int)Mathf.Sign(dir.x), i)] != null)
+                        m_attackNodes.Add(Position.grid + new Vector2Int(j * -(int)Mathf.Sign(dir.x), i));
+                }
+            }
+        }
+        else if (Mathf.Abs(dir.y) > 0)
+        {
+            //facing Up
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = 2; j < 4; j++)
+                {
+                    if (App.GetModule<LevelModule>().CurrentRoom[Position.grid + new Vector2Int(i, j * -(int)Mathf.Sign(dir.y))] != null)
+                        m_attackNodes.Add(Position.grid + new Vector2Int(i, j * -(int)Mathf.Sign(dir.y)));
+                }
             }
         }
 
-        //facing Up
-        for (int i = -1; i < 1; i++)
+        foreach (var node in m_attackNodes)
         {
-            for (int j = 2; j < 3; j++)
+            if (node != null && App.GetModule<LevelModule>().CurrentRoom[node] != null)
             {
-                var spawnPos = Position.grid + new Vector2Int(i, j * sign);
+                App.GetModule<LevelModule>().telegraphDrawer.CreateTelegraph(App.GetModule<LevelModule>().CurrentRoom[node], TelegraphDrawer.Type.ATTACK);
+                
             }
         }
     }
@@ -198,64 +253,47 @@ public class WrymBossEnity : GridEntity
                 direction = 3;
         }
 
-        Vector2 pos;
+        Vector2Int pos;
 
-        switch (direction)
+        do
         {
-            case 0:
-                // Burrow Right /////////
-                pos = FindBurrowHoz(currentRoom.Width - 1, currentRoom);
+            pos = direction switch
+            {
+                0 => FindBurrowHoz(currentRoom.Width - 1, currentRoom),// Burrow Right /////////
+                1 => FindBurrowVert(currentRoom.Height - 1, currentRoom),// Burrow Up /////////
+                2 => FindBurrowHoz(0, currentRoom),// Burrow Left /////////
+                3 => FindBurrowVert(0, currentRoom),// Burrow Down /////////
+                _ => Vector2Int.zero,
+            };
+        } while (currentRoom[pos] == null);
 
-                break;
-
-            case 1:
-                // Burrow Up /////////
-                pos = FindBurrowVert(0, currentRoom);
-                break;
-
-            case 2:
-                // Burrow Left /////////
-                pos = FindBurrowHoz(0, currentRoom);
-                break;
-
-            case 3:
-                // Burrow Down /////////
-                pos = FindBurrowVert(currentRoom.Height - 1, currentRoom);
-
-                break;
-
-            default:
-                pos = Vector2.zero;
-                break;
-        }
-
-        Debug.Log(pos);
+        m_burrowPos = pos;
     }
 
-    private Vector2 FindBurrowHoz(int x, JUtil.Grids.Grid<GridNode> currentRoom)
+    private Vector2Int FindBurrowHoz(int x, JUtil.Grids.Grid<GridNode> currentRoom)
     {
-        List<Vector2> exitPos = new List<Vector2>();
+        List<Vector2Int> exitPos = new List<Vector2Int>();
 
         for (int i = 0; i < currentRoom.Height; i++)
         {
-            if (currentRoom.NodeExists(x, i))
+            if (currentRoom.NodeExists(x, i) && currentRoom[x, i].IsTraversable())
             {
-                exitPos.Add(new Vector2(x, i));
+                exitPos.Add(new Vector2Int(x, i));
             }
         }
 
         return exitPos[Random.Range(0, exitPos.Count)];
     }
 
-    private Vector2 FindBurrowVert(int y, JUtil.Grids.Grid<GridNode> currentRoom)
+    private Vector2Int FindBurrowVert(int y, JUtil.Grids.Grid<GridNode> currentRoom)
     {
-        List<Vector2> exitPos = new List<Vector2>();
+        List<Vector2Int> exitPos = new List<Vector2Int>();
 
         for (int i = 0; i < currentRoom.Width; i++)
         {
-            if (currentRoom.NodeExists(i, y))
+            if (currentRoom.NodeExists(i, y) && currentRoom[i, y].IsTraversable())
             {
-                exitPos.Add(new Vector2(i, y));
+                exitPos.Add(new Vector2Int(i, y));
             }
         }
 
