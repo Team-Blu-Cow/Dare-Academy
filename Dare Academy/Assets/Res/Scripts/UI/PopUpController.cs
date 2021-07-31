0c35c1da0295c67ad650a2e8f2c43956921742af
jack leslie <60219781+JackLeslie432@users.noreply.github.com>
2021-07-31 14:11:30 +0100

    Pop up has multiple screens
    Multiple screen and editable text from the popup controller

--- Dare Academy/Assets/Res/Scripts/UI/PopUpController.cs
+++ Dare Academy/Assets/Res/Scripts/UI/PopUpController.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Playables;
+using TMPro;
+using blu;
+using UnityEngine.UI;
 
 public class PopUpController : MonoBehaviour
 {
     private PlayableDirector m_playableDirector;
+
+    [SerializeField] private string m_head;
+    [SerializeField] private List<string> m_body;
+
+    private bool m_nextDialogue = false;
 
     private void OnValidate()
     {
@@ -22,22 +31,69 @@ public class PopUpController : MonoBehaviour
     {
     }
 
+    private void OnEnable()
+    {
+        Transform textTransform = transform.parent.GetChild(0);
+
+        textTransform.GetComponentsInChildren<TextMeshProUGUI>()[0].text = m_head;
+        textTransform.GetComponentsInChildren<TextMeshProUGUI>()[1].text = m_body[0];
+
+        App.GetModule<InputModule>().DialogueController.Dialogue.Skip.performed += SkipDialogue;
+    }
+
+    private void OnDisable()
+    {
+        App.GetModule<InputModule>().DialogueController.Dialogue.Skip.performed -= SkipDialogue;
+    }
+
     public void StartPopUp()
     {
         StopTimeline();
 
-        //Do convo
+        StartCoroutine(ShowPopup());
+    }
 
+    private IEnumerator ShowPopup()
+    {
+        int i = 1;
+        while (i < m_body.Count)
+        {
+            yield return StartCoroutine(WaitForKeyDown());
+
+            transform.parent.GetChild(0).GetComponentsInChildren<TextMeshProUGUI>()[1].text = m_body[i];
+
+            Canvas.ForceUpdateCanvases();
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform.parent.GetChild(0));
+
+            i++;
+        }
+
+        yield return StartCoroutine(WaitForKeyDown());
         StartTimeline();
+    }
+
+    private IEnumerator WaitForKeyDown()
+    {
+        while (!m_nextDialogue)
+            yield return null;
+
+        m_nextDialogue = false;
+    }
+
+    private void SkipDialogue(InputAction.CallbackContext ctx)
+    {
+        m_nextDialogue = true;
     }
 
     private void StartTimeline()
     {
         m_playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(1);
+        App.GetModule<InputModule>().DialogueController.Dialogue.Disable();
     }
 
     private void StopTimeline()
     {
         m_playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(0);
+        App.GetModule<InputModule>().DialogueController.Dialogue.Enable();
     }
 }
