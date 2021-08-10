8c9645a85c5547b38517bb403c5764c11470c018
jack leslie <60219781+JackLeslie432@users.noreply.github.com>
2021-08-10 14:32:17 +0100

    Changed ingame canvases
    ingame canvases have correct options
    Added cancel ability to controls menu

--- Dare Academy/Assets/Res/Scripts/UI/Controls.cs
+++ Dare Academy/Assets/Res/Scripts/UI/Controls.cs
@@ -63,21 +63,22 @@ public class Controls : MonoBehaviour
     private void SetKeyboard()
     {
         Transform baseTransform = transform.GetChild(2);
-        transform.GetChild(1).GetComponent<Image>().sprite = KeyboardSprites[12];
+        transform.GetChild(1).GetComponent<Image>().sprite = KeyboardSprites[13];
 
-        baseTransform.GetComponentsInChildren<Image>()[0].sprite = KeyboardSprites[9];
-        baseTransform.GetComponentsInChildren<Image>()[1].sprite = KeyboardSprites[2];
-        baseTransform.GetComponentsInChildren<Image>()[2].sprite = KeyboardSprites[8];
-        baseTransform.GetComponentsInChildren<Image>()[3].sprite = KeyboardSprites[7];
-        baseTransform.GetComponentsInChildren<Image>()[4].sprite = KeyboardSprites[15];
-        baseTransform.GetComponentsInChildren<Image>()[5].sprite = KeyboardSprites[16];
-        baseTransform.GetComponentsInChildren<Image>()[6].sprite = KeyboardSprites[0];
-        baseTransform.GetComponentsInChildren<Image>()[7].sprite = KeyboardSprites[1];
-        baseTransform.GetComponentsInChildren<Image>()[8].sprite = KeyboardSprites[11];
-        baseTransform.GetComponentsInChildren<Image>()[9].sprite = KeyboardSprites[13];
-        baseTransform.GetComponentsInChildren<Image>()[10].sprite = KeyboardSprites[14];
-        baseTransform.GetComponentsInChildren<Image>()[11].sprite = KeyboardSprites[14];
-        baseTransform.GetComponentsInChildren<Image>()[12].sprite = KeyboardSprites[10];
+        baseTransform.GetComponentsInChildren<Image>()[0].sprite = KeyboardSprites[10];     // Move up (W)
+        baseTransform.GetComponentsInChildren<Image>()[1].sprite = KeyboardSprites[3];      // Move left (A)
+        baseTransform.GetComponentsInChildren<Image>()[2].sprite = KeyboardSprites[9];      // Move down (S)
+        baseTransform.GetComponentsInChildren<Image>()[3].sprite = KeyboardSprites[8];      // Move right (D)
+        baseTransform.GetComponentsInChildren<Image>()[4].sprite = KeyboardSprites[16];     // Wait (Q)
+        baseTransform.GetComponentsInChildren<Image>()[5].sprite = KeyboardSprites[17];     // Ability Mode (SPC)
+        baseTransform.GetComponentsInChildren<Image>()[6].sprite = KeyboardSprites[0];      // Left Ability (1)
+        baseTransform.GetComponentsInChildren<Image>()[7].sprite = KeyboardSprites[1];      // Right Ability (2)
+        baseTransform.GetComponentsInChildren<Image>()[8].sprite = KeyboardSprites[12];     // Pause (ESC)
+        baseTransform.GetComponentsInChildren<Image>()[9].sprite = KeyboardSprites[14];     // Map (M)
+        baseTransform.GetComponentsInChildren<Image>()[10].sprite = KeyboardSprites[15];    // Zoom map In (SCR WHL)
+        baseTransform.GetComponentsInChildren<Image>()[11].sprite = KeyboardSprites[15];    // Zoom map Out (SCR WHL)
+        baseTransform.GetComponentsInChildren<Image>()[12].sprite = KeyboardSprites[11];    // Interact (E)
+        baseTransform.GetComponentsInChildren<Image>()[13].sprite = KeyboardSprites[2];    // Cancel Ability (BCK SPC)
     }
 
     private void SetController()
@@ -85,18 +86,19 @@ public class Controls : MonoBehaviour
         Transform baseTransform = transform.GetChild(2);
         transform.GetChild(1).GetComponent<Image>().sprite = ControllerSprites[1];
 
-        baseTransform.GetComponentsInChildren<Image>()[0].sprite = ControllerSprites[8];
-        baseTransform.GetComponentsInChildren<Image>()[1].sprite = ControllerSprites[6];
-        baseTransform.GetComponentsInChildren<Image>()[2].sprite = ControllerSprites[5];
-        baseTransform.GetComponentsInChildren<Image>()[3].sprite = ControllerSprites[7];
-        baseTransform.GetComponentsInChildren<Image>()[4].sprite = ControllerSprites[14];
-        baseTransform.GetComponentsInChildren<Image>()[5].sprite = ControllerSprites[13];
-        baseTransform.GetComponentsInChildren<Image>()[6].sprite = ControllerSprites[2];
-        baseTransform.GetComponentsInChildren<Image>()[7].sprite = ControllerSprites[3];
-        baseTransform.GetComponentsInChildren<Image>()[8].sprite = ControllerSprites[10];
-        baseTransform.GetComponentsInChildren<Image>()[9].sprite = ControllerSprites[12];
-        baseTransform.GetComponentsInChildren<Image>()[10].sprite = ControllerSprites[9];
-        baseTransform.GetComponentsInChildren<Image>()[11].sprite = ControllerSprites[11];
-        baseTransform.GetComponentsInChildren<Image>()[12].sprite = ControllerSprites[0];
+        baseTransform.GetComponentsInChildren<Image>()[0].sprite = ControllerSprites[9];    // Move up (D-PAD Up)
+        baseTransform.GetComponentsInChildren<Image>()[1].sprite = ControllerSprites[7];    // Move left (D-PAD left)
+        baseTransform.GetComponentsInChildren<Image>()[2].sprite = ControllerSprites[6];    // Move down (D-PAD down)
+        baseTransform.GetComponentsInChildren<Image>()[3].sprite = ControllerSprites[8];    // Move right (D-PAD right)
+        baseTransform.GetComponentsInChildren<Image>()[4].sprite = ControllerSprites[15];   // Wait (Y)
+        baseTransform.GetComponentsInChildren<Image>()[5].sprite = ControllerSprites[14];   // Ability Mode (X)
+        baseTransform.GetComponentsInChildren<Image>()[6].sprite = ControllerSprites[2];    // Left Ability (RB)//
+        baseTransform.GetComponentsInChildren<Image>()[7].sprite = ControllerSprites[3];    // Right Ability (LB)//
+        baseTransform.GetComponentsInChildren<Image>()[8].sprite = ControllerSprites[11];   // Pause (Menu)
+        baseTransform.GetComponentsInChildren<Image>()[9].sprite = ControllerSprites[13];   // Map (Select)
+        baseTransform.GetComponentsInChildren<Image>()[10].sprite = ControllerSprites[10];   // Zoom map In (LT)
+        baseTransform.GetComponentsInChildren<Image>()[11].sprite = ControllerSprites[12];  // Zoom map Out (RT)
+        baseTransform.GetComponentsInChildren<Image>()[12].sprite = ControllerSprites[0];   // Interact (A)//
+        baseTransform.GetComponentsInChildren<Image>()[13].sprite = ControllerSprites[4];  // Cancel Ability (B)
     }
 }
