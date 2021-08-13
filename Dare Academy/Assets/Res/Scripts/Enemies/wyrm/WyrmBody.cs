4b156171a00fa86d9ed3c27908da94a1fd3d03c9
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-08-13 05:15:04 +0100

    wyrm head animations

--- Dare Academy/Assets/Res/Scripts/Enemies/wyrm/WyrmBody.cs
+++ Dare Academy/Assets/Res/Scripts/Enemies/wyrm/WyrmBody.cs
@@ -135,51 +135,6 @@ public class WyrmBody : WyrmSection
         animator.SetBool("movingRight", false);
     }
 
-    private void SetAnimationFlags(Vector3 start_pos, Vector3 end_pos)
-    {
-        Vector3 diff = end_pos - start_pos;
-
-        if (diff.magnitude < 0.7)
-        {
-            return;
-            // ZeroAnimationFlags();
-        }
-
-        Quaternion rot = Quaternion.identity;
-
-        if (diff.x > 0.7)
-        {
-            animator.SetBool("movingUp", false);
-            animator.SetBool("movingDown", false);
-            animator.SetBool("movingLeft", false);
-            animator.SetBool("movingRight", true);
-            rot = Quaternion.Euler(0, 180, 0);
-        }
-        if (diff.x < -0.7)
-        {
-            animator.SetBool("movingUp", false);
-            animator.SetBool("movingDown", false);
-            animator.SetBool("movingLeft", true);
-            animator.SetBool("movingRight", false);
-        }
-        if (diff.y > 0.7)
-        {
-            animator.SetBool("movingUp", true);
-            animator.SetBool("movingDown", false);
-            animator.SetBool("movingLeft", false);
-            animator.SetBool("movingRight", false);
-        }
-        if (diff.y < -0.7)
-        {
-            animator.SetBool("movingUp", false);
-            animator.SetBool("movingDown", true);
-            animator.SetBool("movingLeft", false);
-            animator.SetBool("movingRight", false);
-        }
-
-        transform.rotation = rot;
-    }
-
     public override void EndStep()
     {
         base.EndStep();
