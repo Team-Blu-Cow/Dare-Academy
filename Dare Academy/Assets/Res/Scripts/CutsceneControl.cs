cd11ab76bf34afd3dad31ea6c2529d3d51f9ee47
sandycfisher <1801486@uad.ac.uk>
2021-08-14 16:54:21 +0100

    Changes in crashsite top

--- Dare Academy/Assets/Res/Scripts/CutsceneControl.cs
+++ Dare Academy/Assets/Res/Scripts/CutsceneControl.cs
@@ -5,11 +5,31 @@ using blu;
 
 public class CutsceneControl : MonoBehaviour
 {
+    private float m_timer;
+
     private void OnDisable()
     {
         if (App.GetModule<SceneModule>())
         {
             App.GetModule<SceneModule>().SwitchScene(LevelModule.ResolveSceneNameString(blu.LevelID._default));
         }
+    }
+
+    private void Start()
+    {
+        m_timer = 0.0f;
+    }
+
+    private void Update()
+    {
+        if(m_timer > 24.5f)
+        {
+            if (App.GetModule<SceneModule>())
+            {
+                App.GetModule<SceneModule>().SwitchScene(LevelModule.ResolveSceneNameString(blu.LevelID._default));
+            }
+        }
+
+        m_timer += Time.deltaTime;
     }
 }
